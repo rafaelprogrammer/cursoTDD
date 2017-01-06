65eebc38ea3dc2501ddb7b81e844e340cebbcad6
rafael <rafael.machado@camara.gov.br>
2017-01-06 16:11:43 -0200

    TDD parte 1

---
+++ pilha/src/main/java/br/com/pilha/Pilha.java
@@ -0,0 +1,28 @@
+package br.com.pilha;
+
+public class Pilha {
+	
+	private Object elemento;
+	private int quantidade;
+
+	public Pilha() {
+	}
+
+	public boolean estaVazia() {
+		return elemento == null;
+	}
+
+	public int tamanho() {
+		return quantidade;
+	}
+
+	public void empilha(Object elemento) {
+		this.elemento = elemento;
+		quantidade++;
+	}
+
+	public Object topo() {
+		return elemento;
+	}
+
+}
