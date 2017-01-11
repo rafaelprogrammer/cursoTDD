2193c3612027408ff394015e6800a8c3e9133d1c
rafael <rafael.machado@camara.gov.br>
2017-01-11 14:18:46 -0200

    2 cilco

--- CamelCase/src/test/java/br/com/caracteres/CamelCase/CamelCaseUtil.java
+++ CamelCase/src/test/java/br/com/caracteres/CamelCase/CamelCaseUtil.java
@@ -1,6 +1,7 @@
 package br.com.caracteres.CamelCase;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CamelCaseUtil {
@@ -12,19 +13,17 @@ public class CamelCaseUtil {
 
 	private List<String> criarLista(String original) {
 		List<String> list = new ArrayList<String>();
-		list.add(original.toLowerCase());
+		list.addAll(Arrays.asList(converter(original)));
 		return list;
 	}
-	
-	/*static String splitCamelCase(String s) {
-		   return s.replaceAll(
-		      String.format("%s|%s|%s",
-		         "(?<=[A-Z])(?=[A-Z][a-z])",
-		         "(?<=[^A-Z])(?=[A-Z])",
-		         "(?<=[A-Za-z])(?=[^A-Za-z])"
-		      ),
-		      " "
-		   );
-		}*/
+
+	private String[] converter(String original) {
+		String[] arrayOriginal = original.replaceAll(String.format("%s|%s|%s", "(?<=[A-Z])(?=[A-Z][a-z])", "(?<=[^A-Z])(?=[A-Z])",
+				"(?<=[A-Za-z])(?=[^A-Za-z])"), " ").split(" ");
+		for(int i=0;i<arrayOriginal.length;i++){
+			arrayOriginal[i] = arrayOriginal[i].substring(0, 1).toLowerCase()+arrayOriginal[i].substring(1);
+		}
+		return arrayOriginal;
+	}
 
 }
--- CamelCase/src/test/java/br/com/caracteres/CamelCase/TestConverterCamelCase.java
+++ CamelCase/src/test/java/br/com/caracteres/CamelCase/TestConverterCamelCase.java
@@ -1,6 +1,7 @@
 package br.com.caracteres.CamelCase;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -11,5 +12,12 @@ public class TestConverterCamelCase {
 		CamelCaseUtil camelCase = new CamelCaseUtil();
 		assertEquals(1,camelCase.converterCamelCase("Rafael").size());
 		assertEquals("rafael",camelCase.converterCamelCase("Rafael").get(0));
+	}
+	
+	@Test
+	public void testConverterNomeComposto(){
+		CamelCaseUtil camelCase = new CamelCaseUtil();
+		assertEquals(2,camelCase.converterCamelCase("RafaelAlves").size());
+		assertArrayEquals(new Object[]{"rafael","alves"}, camelCase.converterCamelCase("RafaelAlves").toArray());
 	}
 }
